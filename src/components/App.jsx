508cafdfa4ae3a99d585a9ee87c68acf387a27c2
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-06-23 13:34:17 +0300

    Update aplication

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,9 @@ export const App = () => {
     </div>
   );
 };
+
+
+
+// Api Key = 9f483b2fd7e828c71a7a1dd0e1411689
+
+// https://api.themoviedb.org/3/movie/550?api_key=9f483b2fd7e828c71a7a1dd0e1411689
