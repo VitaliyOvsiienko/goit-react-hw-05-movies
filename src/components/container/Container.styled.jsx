4bd83e8dcbe3821d7b8500438e3454f813f7492b
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-06-25 11:06:47 +0300

    new components

--- src/components/container/Container.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const StyledContainer = styled.div`
-  max-width: 1600px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: 20px;
-  padding-right: 20px;
-`;
